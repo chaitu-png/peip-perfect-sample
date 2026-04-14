e188fee8963a9a107944fee05cc754fa1d79a62b
chaitu-png <schaitu1706@gmail.com>
2026-04-15 00:02:44 +0530

    Add say_hello function for greeting

---
+++ main.py
@@ -0,0 +1,6 @@
+def say_hello():
+      """Returns a professional greeting."""
+      return "Hello, Predictive Engineering Intelligence Platform!"
+
+if __name__ == "__main__":
+      print(say_hello())
